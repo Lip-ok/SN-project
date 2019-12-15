a8c94bec7fe6d512d88fffafbee96baf00d5d4c4
Lipich <li98pen15sa09sha@gmail.com>
2019-12-15 23:02:27 +0300

    Lesson 21, dialog-page is not work

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-//import s from './Dialogs.module.css'
+import s from './Dialogs.module.css';
 
 
 const Dialogs = () => {
-    return(
+    return (
 
-        <div  className='app-wrapper-content'>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog}>
+                   <span>Siniy</span>
+                </div>
+                <div className={s.dialog}>
+                    Master
+                </div>
+                <div className={s.dialog}>
+                    Maks
+                </div>
+                <div className={s.dialog}>
+                    Kotova
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.dialog}>
+                    Hi!
+                </div>
+                <div className={s.dialog}>
+                    Hello!
+                </div>
+                <div className={s.dialog}>
+                    Yo!
+                </div>
+            </div>
         </div>
     )
-}
+};
 export default Dialogs;
