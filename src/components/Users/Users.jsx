bd8a76dd6627914ca398262d1e88ce3f71077642
Lipich <li98pen15sa09sha@gmail.com>
2020-01-28 18:25:55 +0300

    Lesson 54 completed

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,12 @@ import userPhoto from "../../assets/image/userPhoto.jpg";
 
 
 class Users extends React.Component {
-   constructor(props) {
-       super(props);
-       axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-           this.props.setUsers(response.data.items);
-           alert('Upload page!')
-       })
+   componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        this.props.setUsers(response.data.items);
+    })
    }
+
     render() {
         return (
             <div>
